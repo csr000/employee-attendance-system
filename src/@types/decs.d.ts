1d016a03e74d13ac6a63b6f24f5347d2a636fde3
csr000 <csrichard000@gmail.com>
2022-08-27 20:35:58 +0100

    🎨 add types

--- src/@types/decs.d.ts
+++ src/@types/decs.d.ts
@@ -3,3 +3,19 @@ declare module 'assets/*'
 declare module 'components/*'
 declare module 'routes'
 declare module 'chart.js'
+
+export declare type Attendance = {
+  id: string
+  name: string
+  email: string
+  dept: string
+  datetime: string
+}
+
+export declare type Employee = {
+  id: string
+  name: string
+  email: string
+  phone: string
+  dept: string
+}
