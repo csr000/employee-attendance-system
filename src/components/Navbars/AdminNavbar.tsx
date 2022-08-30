ffa80127f50e63ed3e40cd167e7afda84111a847
csr000 <csrichard000@gmail.com>
2022-08-30 03:56:03 +0100

    🎨 Position dashboardForm to top right corner

--- src/components/Navbars/AdminNavbar.tsx
+++ src/components/Navbars/AdminNavbar.tsx
@@ -1,6 +1,68 @@
+import { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 // reactstrap components
-import { Navbar, Container } from 'reactstrap'
+import { Navbar, Container, Row } from 'reactstrap'
+import TimeInput from 'react-widgets/TimeInput'
+import { TITLE } from '../../constants'
+import { Context } from '../../context'
+import { Employee } from '../../@types/decs'
+
+const DashboardForm = () => {
+  const [value, setValue] = useState()
+  const [selectedLect, setSelectedLect] = useState([])
+  const {emps} = useContext<any>(Context)
+  // CONSTANTS
+  const addATTENDANCE = 'add attendance'
+  return  <Row>
+  {/* add attendance */}
+  <form className="box">
+    <select
+      name="emps"
+      onChange={(e: any) => setSelectedLect(e.target.value)}
+    >
+      <option value="">--Choose {TITLE}--</option>
+      {(emps as Employee[]).map(emp => (
+        <option key={emp.id} value={emp.name}>
+          {emp.name}
+        </option>
+      ))}
+    </select>
+    <TimeInput
+      use12HourClock
+      // @ts-ignore
+      defaultValue={new Date()}
+      onChange={(v: any) => setValue(v?.toString())}
+      style={{ width: 'auto' }}
+    />
+  </form>
+  <button
+    className="button"
+    type="button"
+    onClick={() => {
+      const datetime = value || Date()
+      window.main.sendMessage('ipc-example', [
+        {
+          aim: addATTENDANCE,
+          selectedLect,
+          datetime,
+        },
+      ])
+    }}
+  >
+    <p className="add">Add New</p>
+  </button>
+</Row>
+}
+
+const EmployeesForm = () => {
+  return <h1>employees</h1>
+}
+
+const getForm = (brandtext: string) => {
+  if (brandtext === "Dashboard") { return <DashboardForm/> }
+  else if (brandtext === "Employees") { return <EmployeesForm/> }
+  else { return "" }
+}
 
 const AdminNavbar = (props: any) => {
   return (
@@ -13,7 +75,7 @@ const AdminNavbar = (props: any) => {
           >
             {props.brandText}
           </Link>
-          <h1>Hey</h1>
+          { getForm(props.brandText) }
         </Container>
       </Navbar>
     </>
