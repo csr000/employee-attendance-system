35bdcd083bae7a81dc4fadf34c1b68c4085e19e8
csr000 <csrichard000@gmail.com>
2022-09-14 15:29:41 +0100

    🎨 update constants

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,7 +1,23 @@
-export const TITLE  = "Lecturer" 
-
+export const TITLE = 'Lecturer'
+export const ipcCHANNEL = 'main-channel'
 export const EMP = {
   CREATE: 'create emp',
-  DELETE : 'delete emp',
-  UPDATE : 'update emp'
+  DELETE: 'delete emp',
+  UPDATE: 'update emp',
+}
+
+export const ATT = {
+  ADD: 'add attendance',
+  DELETE: 'delete emp',
+  UPDATE: 'update emp',
+}
+
+export const AUTH = {
+  LOGIN: 'login',
+}
+
+export const REPLIES = {
+  RESETpwd: "reply-resetpwd",
+  LOGIN: "reply-login"
+  
 }
