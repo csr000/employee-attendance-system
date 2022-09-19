01be47e9532d4a399ecca86592e73c8edc592ddf
csr000 <csrichard000@gmail.com>
2022-09-19 02:19:23 +0100

    :package: add bcrypt

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom'
 import { HashRouter, Route, Switch } from 'react-router-dom'
 
+import 'animate.css'
 import './assets/plugins/nucleo/css/nucleo.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './assets/scss/argon-dashboard-react.scss'
 
 import AdminLayout from './layouts/Admin'
 import { ping } from './utils'
-
 
 ping('ping')
 
